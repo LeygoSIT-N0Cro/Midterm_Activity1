23943283bda4e91ce5958db61c9f5439af342b03
LeygoSIT-N0cro <20256409@s.ubaguio.edu>
2026-03-09 12:35:05 +0800

    Home page.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+    return (
+        <div> 
+            <h2>Welcome to Student Dashboard</h2> 
+            <p>This app demonstrates React Router, useState, and useEffect.</p> 
+        </div> 
+
+    );
+}
+export default Home;
