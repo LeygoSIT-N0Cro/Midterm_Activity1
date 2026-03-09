e20683e62b57e670aad5a58222192e3b2c4fc693
LeygoSIT-N0cro <20256409@s.ubaguio.edu>
2026-03-09 13:08:43 +0800

    Created students page

--- src/pages/Students.jsx
+++ src/pages/Students.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react"
+import StudentList from "../components/StudentList"
+
 function Students() {
-    return (           
-    <div className="students-page"></div>
-  );
+
+  const [students, setStudents] = useState([])
+
+  const studentData = [
+    { id:1, name:"Lin", course:"BSIT" },
+    { id:2, name:"Ellen", course:"BSCS" },
+    { id:3, name:"Rina", course:"BSIT" },
+    { id:4, name:"Burnice", course:"BSIS" },
+    { id:5, name:"Jane", course:"BSIT" },
+    { id:6, name:"Belle", course:"BSCS" },
+    { id:7, name:"Wise", course:"BSIT" },
+    { id:8, name:"Astra", course:"BSIS" },
+    { id:9, name:"Evilyn", course:"BSIT" },
+    { id:10, name:"Vivian", course:"BSCS" }
+  ]
+
+  useEffect(() => {
+    setStudents(studentData)
+  }, [])
+
+  return (
+    <div>
+      <h2>Student List</h2>
+
+      {students.map((student) => (
+        <StudentList key={student.id} data={student} />
+      ))}
+
+    </div>
+  )
 }
 
-export default Students;
+export default Students
